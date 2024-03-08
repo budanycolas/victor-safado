390e6b2f489c77309e1fb7e4db486fa1b1ef21b3
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-03-08 09:12:13 -0300

    S

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // alert('bom dia cachorrada!!');
 // alert('NYCOLAS É BROXA!!!');
 
- var nomeUsuario = prompt('qual seu vulgo??');
+ var nomeUsuario = '';
 
 
  var texto = document.querySelector('span');
 
  texto.textContent = nomeUsuario;
- 
+
+
+
+ while(nomeUsuario == ''){
+    var nomeUsuario = prompt('qual seu vulgo??');
+ }
