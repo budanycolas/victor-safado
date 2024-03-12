38c3af812fddde6d8d77ba4bc721969d2747c626
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-03-12 07:56:27 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,1 +1,11 @@
 let numeroSecreto = 7;
+
+let chute = prompt('Ecolha um número entre 0 a 10');
+
+if(numeroSecreto == chute){
+    alert('Boa cpx');
+    console.log('Nice');
+} else{
+    alert('errou cpx');
+    console.log('paia');
+}
