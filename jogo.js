435eab1c05168e92c4b56c7a132ff8d69a642304
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-03-12 08:28:19 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,11 +1,16 @@
-let numeroSecreto = 7;
+let numeroSecreto = Math.random() * 10;
 
 let chute = prompt('Ecolha um número entre 0 a 10');
 
 while(chute == ''){
-    chute = prompt('Escolha um número entre 0 a 10.')
+
+chute = prompt('Escolha um número entre 0 a 10.');
 }
 
+while(chute == null || chute !=numeroSecreto){
+ alert("tente denovo vh 7 ");
+ chute = prompt('Escolha um número entre 0 a 10.');
+}
 if(numeroSecreto == chute){
     alert('Boa cpx');
     console.log('Nice');
