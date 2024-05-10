99d4d9a79c4841b4aa23ca36c437ee981b33f2ff
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-05-10 09:21:01 -0300

    s

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ var nycolas_perdeu tudo = '';
 var texto = document.querySelector('span');
 
 while(nycolas_perdeu == ''){ // || nycolas_perdeu == null
-    nycolas_perdeu = prompt('Quantas vezes a Mahara ja foi presa?');
+    nycolas_perdeu = prompt('Quantas vezes o nycolas perdeu?');
 
 }
 
 if (nycolas_perdeu == null){
-    texto.textContent = 0;
+    texto.textContent = 1000000;
 } else {
     texto.textContent = nycolas_perdeu;
 }
