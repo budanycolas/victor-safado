b447ad4dde2541c7b3d05bfa15cfd06b2141d084
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-05-10 08:41:29 -0300

    ´P

--- jogo.js
+++ jogo.js
@@ -5,19 +5,19 @@
 //while(chute == ''){
 
 //chute = prompt('Escolha um número entre 0 a 10.');
-}
+//}
 
 //while(chute == null || chute !=numeroSecreto){
 // alert("tente denovo vh 7 ");
  //chute = prompt('Escolha um número entre 0 a 10.');
-}
+//}
 //if(numeroSecreto == chute){
    // alert('Boa cpx');
     //console.log('Nice');
 //}else{
    // alert('errou cpx');
    // console.log('paia');
-}
+//}
 
 
 
